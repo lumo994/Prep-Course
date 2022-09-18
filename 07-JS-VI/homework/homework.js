3c303d84db719d666074c904a20c0636fdb14231
Lucia <luciaemorana@gmail.com>
2022-09-18 10:46:48 -0300

    JVI

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,20 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  return nombre[0].toUpperCase() + nombre.slice(1).toLowerCase();
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
-  //Tu código:
+  //Tu código: 
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,25 +25,38 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  const total = numeros.reduce(function(acumulador, numero) {
+    return acumulador + numero;
+  }, 0)
+  cb(total);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(elemento) { cb(elemento) })
 }
+
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var nuevoarray=[];
+  array.map(function(x){
+  nuevoarray.push(cb(x));
+  });
+  return nuevoarray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  const result = array.filter(array => array[0] === "a");
+return result; 
 }
 
 // No modificar nada debajo de esta línea
